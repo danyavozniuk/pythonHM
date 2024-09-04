73c31fe87a173a938e283c02f643610cf51ba89c
Danya <danilvozniuk47@gmail.com>
2024-09-04 21:06:25 +0100

    File HM

---
+++ .venv/ДЗ.py
@@ -0,0 +1,17 @@
+# Створення змінних
+greeting = "hello world"
+name = "Даниїл"
+surname = "Вознюк"
+age = 16
+
+print("Тип змінної 'greeting':", type(greeting))
+print("Значення змінної 'greeting':", greeting)
+
+print("Тип змінної 'name':", type(name))
+print("Значення змінної 'name':", name)
+
+print("Тип змінної 'surname':", type(surname))
+print("Значення змінної 'surname':", surname)
+
+print("Тип змінної 'age':", type(age))
+print("Значення змінної 'age':", age)
